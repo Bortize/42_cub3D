0fd955e04db3ab688451d5469210d90ccb950614
Borja <bgomez-r@student.42madrid.com>
2021-01-08 12:43:14 +0100

    Fixed of plan validation with fportalo

--- srcs/validation/map/boundary_validation/boundary_validation.c
+++ srcs/validation/map/boundary_validation/boundary_validation.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   boundary_validation.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bgomez-r <bgomez-r@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: bgomez-r <bgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 17:59:10 by bgomez-r          #+#    #+#             */
-/*   Updated: 2021/01/05 23:28:18 by bgomez-r         ###   ########.fr       */
+/*   Updated: 2021/01/08 12:38:11 by bgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@
 */
 void	boundary_fill(int x, int y, int fill_value, int boundary_value, map_plan *plan)
 {
-	ft_printf("El algoritmo esta en: %c \n", plan->plan[x][y]);
+	ft_printf("El algoritmo esta en la coordenada %i %i y contiene %c \n", x, y, plan->plan[x][y]);
 	if ((plan->plan[x][y] != 102) && (plan->plan[x][y] != 49) && (plan->plan[x][y] != 50))
 	{
-		put_pixel(plan->player_position_x, plan->player_position_y, plan);
+		put_pixel(x, y, plan);
 		boundary_fill(x + 1, y, fill_value, boundary_value, plan);
 		boundary_fill(x, y + 1, fill_value, boundary_value, plan);
 		boundary_fill(x - 1, y, fill_value, boundary_value, plan);
