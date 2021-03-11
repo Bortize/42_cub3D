291c55c60e96558d24672959592d0fd77632120a
Borja <bgomez-r@student.42madrid.com>
2021-03-11 19:50:27 +0100

    ramdom errors

--- srcs/validation/identifiers/identifiers_validation/screen_validation_r.c
+++ srcs/validation/identifiers/identifiers_validation/screen_validation_r.c
@@ -6,7 +6,7 @@
 /*   By: bgomez-r <bgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 15:54:56 by bgomez-r          #+#    #+#             */
-/*   Updated: 2021/03/10 17:15:03 by bgomez-r         ###   ########.fr       */
+/*   Updated: 2021/03/11 19:34:00 by bgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,18 @@ void	screen_validation_r(t_cub3d *cub)
 {
 	if (ft_strcmp("R", cub->map.tab[0]) == 0)
 	{
+		int	max_widht;
+		int max_height;
+
 		cub->map.flag_widht++;
 		cub->map.i++;
 		integer_validation_resolution(cub);
+		mlx_get_screen_size(cub->mlx.init, &max_widht, &max_height);
 		cub->map.width = ft_atoi(cub->map.tab[1]);
 		cub->map.height = ft_atoi(cub->map.tab[2]);
+		if (cub->map.width > max_widht)
+			cub->map.width = max_widht;
+		if (cub->map.height > max_height)
+			cub->map.height = max_height;
 	}
 }
